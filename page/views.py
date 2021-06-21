930f427907f93a501984eeb411124874ccb0de7c
Frann131 <franmindurry@gmail.com>
2021-06-21 16:49:06 -0300

    html sin css

--- page/views.py
+++ page/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Movie
+
 
 def movie_list(request):
-    return render(request, 'page/movie_list.html', {})
+    movies = Movie.objects.all()
+    return render(request, 'page/movie_list.html', {'movies': movies})
