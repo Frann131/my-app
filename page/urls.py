c9422276a6d0d25202007c86b688b9d6324800c6
Frann131 <franmindurry@gmail.com>
2021-06-24 11:13:46 -0300

    .

--- page/urls.py
+++ page/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .forms import MovieForm
 
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
     path('movie/<int:pk>/', views.movie_detail, name='movie_detail'),
+    path('movie/new', views.movie_new, name='movie_new'),
 ]
--- page/views.py
+++ page/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
-
+from page.forms import MovieForm
 
 def movie_list(request):
     movies = Movie.objects.all()
@@ -9,3 +9,8 @@ def movie_list(request):
 def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
     return render(request, 'page/movie_detail.html', {'movie': movie})
+
+def movie_new(request):
+    form = MovieForm()
+    return render(request, 'page/movie_edit.html', {'form': form})
+
