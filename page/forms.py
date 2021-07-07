ce07c85d4b8ea84497a60f5a6b13d2101b0c3652
JuanMallon <juankamalloncabj@gmail.com>
2021-07-07 20:50:34 -0300

    placeholders

--- page/forms.py
+++ page/forms.py
@@ -7,3 +7,12 @@ class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
         fields = ('originalName', 'nameInSpain', 'year', 'director', 'duration', 'sinopsis', 'genres', 'rate')
+        widgets = {
+            'originalName': forms.TextInput(attrs={'placeholder': 'Nombre orignal de la pelicula'}),
+            'nameInSpain': forms.TextInput(attrs={'placeholder': 'Nombre de la pelicula en España'}),
+            'year': forms.TextInput(attrs={'placeholder': 'Año de lanzamiento'}),
+            'director': forms.TextInput(attrs={'placeholder': 'Nombre del director'}),
+            'duration': forms.TextInput(attrs={'placeholder': 'Minutos'}),
+            'sinopsis': forms.TextInput(attrs={'placeholder': 'Breve resumen de la trama'}),
+            'rate': forms.TextInput(attrs={'placeholder': 'Separados por coma'}),
+        }
