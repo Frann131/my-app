a61f183188b860f2008f999fe4f9bbc9f6dc396c
Frann131 <franmindurry@gmail.com>
2021-07-07 21:40:08 -0300

    fixing

--- page/forms.py
+++ page/forms.py
@@ -8,11 +8,11 @@ class MovieForm(forms.ModelForm):
         model = Movie
         fields = ('originalName', 'nameInSpain', 'year', 'director', 'duration', 'sinopsis', 'genres', 'rate')
         widgets = {
-            'originalName': forms.TextInput(attrs={'placeholder': 'Nombre orignal de la pelicula'}),
+            'originalName': forms.Charinput(attrs={'placeholder': 'Nombre orignal de la pelicula'}),
             'nameInSpain': forms.TextInput(attrs={'placeholder': 'Nombre de la pelicula en España'}),
             'year': forms.TextInput(attrs={'placeholder': 'Año de lanzamiento'}),
             'director': forms.TextInput(attrs={'placeholder': 'Nombre del director'}),
-            'duration': forms.TextInput(attrs={'placeholder': 'Minutos'}),
+            'duration': forms.positive_small_integer_field(attrs={'placeholder': 'Minutos'}),
             'sinopsis': forms.TextInput(attrs={'placeholder': 'Breve resumen de la trama'}),
             'genres': forms.TextInput(attrs={'placeholder': 'Separados por coma'}),
         }
